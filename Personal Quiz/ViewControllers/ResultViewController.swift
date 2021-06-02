d0aee0977e748d4d6c1a853f81d1da3f80a81017
ilabrosimov <ilabrosimov@gmail.com>
2021-06-02 21:35:40 +0300

    Initial commit

---
+++ Personal Quiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultViewController.swift
+//  Personal Quiz
+//
+//  Created by 18992227 on 31.05.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    // 1. Массив ответов
+    // 2. Определить наиболее часто встречаемый тип животного
+    // 3. Отобразить результат
+    // 4. Избавиться от кнопки Back
+    
+    var answers: [Answer] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
